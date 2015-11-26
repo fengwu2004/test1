73baac062f8b7ea735bb09bc9e49169cb0272c61
fengwu2004 <user@users-iMac-2.local>
2015-11-26 14:18:48 +0800

    let go

--- studentController.js
+++ studentController.js
@@ -1,20 +1,7 @@
 mainApp.controller("studentController", function($scope) {
-	
-	$scope.student = { 
-		firstname:"Mahesh",
-		lastname:"parashar",
-		fees:500,
-		subjects:[
-			{name:'physics', marks:70},
-			{name:'chemistry', marks:80},
-			{name:'math', marks:65},
-			{name:'English', marks:75},
-			{name:'Hindi', marks:67},
-		]}
-		
 	$scope.reset = function() {
-			$scope.student.firstname = "Bill";
-			$scope.student.lastname = "Gate";
-			$scope.student.email = "microsoftt@microsoft.com"; 
+			$scope.firstName = "Bill";
+			$scope.lastName = "Gate";
+			$scope.email = "microsoftt@microsoft.com"; 
 	}
 })
